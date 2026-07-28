a34791bf9ac2da6ff2655f0c29280b6aa17f43cc
codebyjhade <ebuanbugauisanbryanjhade@gmail.com>
2026-07-28 21:16:32 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -111,7 +111,7 @@ export default function Header() {
   }
 
   return (
-    <header className="w-full bg-[#6949a8] pt-6 pb-22 relative">
+    <header className="w-full bg-[#6949a8] pt-7 pb-23 relative">
       <div className="max-w-5xl mx-auto px-[25px] flex justify-between items-center gap-4 select-none relative z-30">
         
         {/* Left Column: Greeting & Date */}
