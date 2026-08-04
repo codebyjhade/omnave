d1fc097a688dd1e5ba99148345763ff8f7540de1
codebyjhade <ebuanbugauisanbryanjhade@gmail.com>
2026-08-04 16:05:00 +0800

    clean up

--- components/lesson/index.ts
+++ components/lesson/index.ts
@@ -1,8 +1,3 @@
-export { LessonHeader } from "./LessonHeader";
-export { LessonNav } from "./LessonNav";
-export { StudySessionBanner } from "./StudySessionBanner";
 export { ChatPanel } from "./ChatPanel";
-export { GamificationToast } from "./GamificationToast";
 export { MarkdownRenderer } from "./MarkdownRenderer";
 export { TypewriterText } from "./TypewriterText";
-export type { TabId } from "./LessonNav";
--- components/profile/index.ts
+++
@@ -1,5 +0,0 @@
-export { ProfileHero } from "./ProfileHero";
-export { LearningOverview } from "./LearningOverview";
-export { BadgeShowcase, BadgeArchiveModal } from "./BadgeShowcase";
-export { SubscriptionCard } from "./SubscriptionCard";
-
