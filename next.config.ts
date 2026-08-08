8cbcbc81961c4c395f0079b745a015d6a0d9d2d1
codebyjhade <ebuanbugauisanbryanjhade@gmail.com>
2026-08-08 22:56:26 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,7 @@ const withPWA = withPWAInit({
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['pdf-parse'],
+  turbopack: {}, // <-- ADD THIS EXACT LINE
 };
 
 export default withPWA(nextConfig);
